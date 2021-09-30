37e014acd45366d66f24d10c41a45ef4b5284085
HuCutie <husmallcutie@gmail.com>
2021-09-30 13:58:17 +0800

    Chapter 5.4 Forest (3 notations)

--- Chapter 5 Tree/include/Forest.h
+++ Chapter 5 Tree/include/Forest.h
@@ -5,18 +5,26 @@ using namespace std;
 #define MaxSize 100
 #define ElemType char
 
+// Basic Node
 typedef struct
 {
     ElemType data;
     int parent;
 }PTnode;
 
+// Parent Notation
 typedef struct
 {
     PTnode nodes[MaxSize];
     int n;
 }PTree;
 
+// Child Notation
+/*
+    This notation uses a linked list to connect all nodes. 
+*/
+
+// Child-Sibling Notation
 typedef struct CSNode
 {
     ElemType data;
